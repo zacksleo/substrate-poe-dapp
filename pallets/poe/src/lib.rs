31ef50fb978eb1d525260e7401ae33cf5594ab95
zacksleo <zacksleo@gmail.com>
2021-07-31 15:20:00 +0800

    initial commit

---
+++ pallets/poe/src/lib.rs
@@ -0,0 +1,91 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{dispatch::DispatchResult, pallet_prelude::*, sp_std::vec::Vec};
+	use frame_system::pallet_prelude::*;
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
+	pub type Proofs<T: Config> =
+		StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+
+	#[pallet::event]
+	#[pallet::metadata(T::AccountId = "AccountId")]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		ClaimCreated(T::AccountId, Vec<u8>),
+		ClaimRemoked(T::AccountId, Vec<u8>),
+		ClaimTransfered(T::AccountId, T::AccountId, Vec<u8>),
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		ProofAlreadyClaimed,
+		NoSuchProof,
+		NotProofOfOwner,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn create_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+			Proofs::<T>::insert(&proof, (&who, <frame_system::Pallet<T>>::block_number()));
+
+			Self::deposit_event(Event::ClaimCreated(who, proof));
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+			let (owner, _) = Proofs::<T>::get(&proof);
+			ensure!(owner == who, Error::<T>::NotProofOfOwner);
+
+			Proofs::<T>::remove(&proof);
+
+			Self::deposit_event(Event::ClaimRemoked(who, proof));
+			Ok(())
+		}
+
+		#[pallet::weight(20_000 + T::DbWeight::get().writes(1))]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			proof: Vec<u8>,
+			receiver: T::AccountId,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+			let (owner, block_num) = Proofs::<T>::get(&proof);
+			ensure!(owner == sender, Error::<T>::NotProofOfOwner);
+
+			Proofs::<T>::remove(&proof);
+			Proofs::<T>::insert(&proof, (&receiver, block_num));
+
+			Self::deposit_event(Event::ClaimTransfered(sender, receiver, proof));
+
+			Ok(())
+		}
+	}
+}
